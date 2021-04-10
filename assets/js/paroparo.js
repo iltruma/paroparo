1062837c1b22855001099eb6ccf6e3b1ddcb1835
iltruma <casini.cosimo@gmail.com>
2021-04-10 23:13:17 +0200

    Fix sfondo macaw

--- assets/js/paroparo.js
+++ assets/js/paroparo.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         }
   });
 
-  if($('#mapid')) {
+  if($('#mapid').length) {
 	var mymap = L.map('mapid').setView([43.598508, 11.468536], 13);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -26,15 +26,15 @@ $(document).ready(function () {
 	}).addTo(mymap);
 
     var circle = L.circle([43.598508, 11.468536], {
-        color: '#FF6933',
-        fillColor: '#FF6933',
+        color: '#186e64',
+        fillColor: '#186e64',
         fillOpacity: 0.3,
         radius: 2000
     }).addTo(mymap);
     circle.bindPopup('<div class="container"><div class="row"><div class="col-12"><h4>{{site.title}}</h4><p class="mt-0 mb-2">{{site.description | escape}}</p><h6 class="mb-0">Indirizzo</h6><address>{{site.address}}<br>{{site.city}}</address><a target="_blank" href="{{site.url}}">Sito Web</a></div></div></div>').openPopup();
   }
 
-  if($('#pageclip-form')) {
+  if($('#pageclip-form').length) {
       var form = $('#pageclip-form');
       Pageclip.form(form, {
         successTemplate: '<span>Grazie della richiesta!<span class="mdi mdi-heart"></span></span>'
